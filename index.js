476180c8e34f42691ce3b250a0f9f74e92c028d5
LeeMinGyu <skystar9927@naver.com>
2023-11-08 20:03:16 +0900

    no message

--- index.js
+++ index.js
@@ -37,9 +37,9 @@ function add_Q(){
     const newDiv = document.createElement('div');
     let cnt_QA = QA_section.childElementCount+1
     newDiv.setAttribute("id","question_"+cnt_QA)
-    newDiv.setAttribute("style","background-color: rgb(216, 224, 255); margin-top: 10px;")
+    newDiv.setAttribute("style","background-color: rgb(227, 227, 227); margin-top: 10px;")
 
-    newDiv.innerHTML='<input type="text" value="질문'+cnt_QA+'" placeholder="질문'+cnt_QA+'"><div><select name="" id=""><option value="1" name="MCQ">객관식</option><option value="2" name="srt_ansr">단답형</option><option value="3" name="essay">서술형</option></select></div><label><input type="checkbox">중복선택 허용</label><div id="choice_opt_section"><div><input type="text" value="1번 보기" placeholder="1번 보기"><input type="button" value="보기 삭제" onclick="del(this)"></div><div><input type="text" value="2번 보기" placeholder="2번 보기"><input type="button" value="보기 삭제" onclick="del(this)"></div></div><input type="button" value="보기 추가" onclick="add_opt()"><input type="button" value="질문 삭제" onclick="del(this)">'
+    newDiv.innerHTML='<input type="text" value="질문'+cnt_QA+'" placeholder="질문'+cnt_QA+'"><div><select id="select_value" onchange="change_form(this.value)"><option value="multi" name="MCQ">객관식</option><option value="short" name="srt_ansr">단답형</option><option value="descript" name="descriptive">서술형</option></select></div><div id="change_section"> <!-- 객관식/단답형/서술형 바뀌는 부분 --><label><input type="checkbox">중복선택 허용</label><div id="choice_opt_section"><div><input type="text" value="1번 보기" placeholder="1번 보기"><input type="button" value="보기 삭제" onclick="del(this)"></div><div><input type="text" value="2번 보기" placeholder="2번 보기"><input type="button" value="보기 삭제" onclick="del(this)"></div></div><input type="button" value="보기 추가" onclick="add_opt()"></div><input type="button" value="질문 삭제" onclick="del(this)">'
     QA_section.appendChild(newDiv)
 }
 
